42462284e23c9120709e153575afeba7c20a5d7b
pWn3d <winkler.gernot@gmail.com>
2023-04-07 17:17:34 +0200

    Update to 1.19.3 completed

--- src/main/java/techguns/client/render/math/TGMatrixOps.java
+++ src/main/java/techguns/client/render/math/TGMatrixOps.java
@@ -3,7 +3,6 @@ package techguns.client.render.math;
 import net.minecraft.client.util.math.MatrixStack;
 import org.joml.AxisAngle4f;
 import org.joml.Quaternionf;
-import techguns.client.render.TGRenderHelper;
 
 public class TGMatrixOps {
 	private static final float DEG2RAD = (float) (Math.PI / 180.0);
